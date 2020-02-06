30d99ea418886049f0559cee6c959f3b73bc5206
GunterPearson <1715@holbertonschool.com>
2020-02-06 20:22:28 +0000

    shows last digit of string and tells whether it is greater than 5, lower than 6, or equal to 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+B#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /*
@@ -23,6 +23,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	int m = n % 10;
+
 	printf("Last digit of %i is %i ", n, m);
 	if (m > 5)
 		printf("and is greater than 5\n");
