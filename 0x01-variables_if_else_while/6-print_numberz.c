dc3e478d3fee5d4a93662ef98997f75c8575fa55
GunterPearson <1715@holbertonschool.com>
2020-02-06 22:12:01 +0000

    program that writes 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * 5-print_numbers.c
+ *
+ * description:
+ * prints out all single digit
+ * of base 10
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char single;
+
+	single = '0';
+	for (single = '0'; single <= '9'; single++)
+		putchar(single);
+	putchar('\n');
+	return (0);
+}
