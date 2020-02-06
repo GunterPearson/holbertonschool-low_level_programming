80fdb99bfffbfe900701639a1609f1dca3f819c8
GunterPearson <1715@holbertonschool.com>
2020-02-06 00:51:13 +0000

    using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - starting point for program
+ * return - 0 (success)
+ */
+
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
-	return(0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
