9e07928e49b21ebb380e4d24c2e4576837b21192
GunterPearson <1715@holbertonschool.com>
2020-02-07 00:37:29 +0000

    displays 00-99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * 10-print_comb.c
+ *
+ * description:
+ * prints out all
+ * digit combinations
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 100; b++)
+	{
+		putchar((b/10)+'0');
+		putchar((b%10)+'0');
+		if (!(b == 99))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
