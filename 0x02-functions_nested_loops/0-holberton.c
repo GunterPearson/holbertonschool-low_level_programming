d2bd499d72a93dcea1b3e8ed5e77a2a557bee142
GunterPearson <1715@holbertonschool.com>
2020-02-10 17:07:48 +0000

    commited readme and holberton 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+ * 0-hoberton.c
+ *
+ * description:
+ * prints out holberton
+ * using putchar
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+
+	return (0);
+}
