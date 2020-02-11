b56153cd773ba07446a80c181f715fd0127db193
GunterPearson <1715@holbertonschool.com>
2020-02-11 06:44:12 +0000

    func 10 adds together two ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/*
+ * 9-times_table.c
+ *
+ * description:
+ * writes a function
+ * that computes
+ * 9 time table
+ */
+
+/**
+ * times_table- start
+ * @a -placeholder for int
+ * @b -placeholder for int2
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
