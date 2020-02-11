418686d670aae482912fc10dff08c7bb0c7ce033
GunterPearson <1715@holbertonschool.com>
2020-02-11 15:21:16 +0000

    corrrected betty standards

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -11,9 +11,9 @@
  */
 
 /**
- * times_table- start
- * @a -placeholder for int
- * @b -placeholder for int2
+ * add- start
+ * @a: placeholder for int
+ * @b: placeholder for int2
  * Return: Always 0 (Success)
  */
 int add(int a, int b)
