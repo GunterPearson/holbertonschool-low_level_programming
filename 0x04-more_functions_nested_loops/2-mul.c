3f15f2fda70b4b2ef667fc9c9049e96a3b313803
GunterPearson <1715@holbertonschool.com>
2020-02-13 17:43:11 +0000

    updated header file and add 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - start of function
+ * @a: first int
+ * @b: second int
+ *
+ * Return: 0 is success
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
