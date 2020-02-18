ff72af4b6f62be44424cc0c308ec6ee8b06be5e5
GunterPearson <1715@holbertonschool.com>
2020-02-18 20:53:29 +0000

    added file 8 and updated head file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_array - starting point
+ * @a: place holder
+ * @n: place holder
+ *
+ */
+void print_array(int *a, int n)
+{
+	int p;
+
+	for (p = 0; p < n; p++)
+	{
+		if (p != n - 1)
+			printf("%d, ", a[p]);
+		else
+		printf("%d\n", a[p]);
+	}
+}
