659d61c1205f90f9fd5b6d58c29a20b00538be2d
GunterPearson <1715@holbertonschool.com>
2020-02-18 19:07:12 +0000

    corrected number 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - start of function
+ * @s: place holder for string
+ *
+ */
+void rev_string(char *s)
+{
+	int i, l;
+	char a, *e = s, *b = s;
+
+        for (i = 0; s[i] != '\0'; i++)
+		;
+	l = i;
+	for (i = 0; i < l - 1; i++)
+		e++;
+	for (i = 0; i < l / 2; i++)
+	{
+		a = *b;
+		*b = *e;
+		*e = a;
+		b++;
+		e--;
+	}
+}
