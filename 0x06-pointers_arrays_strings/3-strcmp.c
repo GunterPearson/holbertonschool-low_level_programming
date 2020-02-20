7139288cd52ff0c170bf3fe131e436b8271e50cb
GunterPearson <1715@holbertonschool.com>
2020-02-20 20:31:00 +0000

    correcting 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int c1, c2, d;
-
+	int a, b;
 	for (c1 = 0; s1[c1] != '\0'; c1++)
-		;
+		a = s1[c1] + '0';
 	for (c2 = 0; s2[c2] != '\0'; c2++)
-		;
-	d = c1 - c2;
+		b = s2[c2] + '0';
+	d = a - b;
 	return (d);
 }
