c5eaa3a544023983978694f1a88b4321366dc830
GunterPearson <1715@holbertonschool.com>
2020-02-20 19:29:57 +0000

    added file 5 and updated header file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - entry of function
+ * @s: string of characters we are given
+ *
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 'a' && s[i] < 'z')
+		{
+			s[i] -= 32;
+		}
+	}
+       	return (s);
+}
