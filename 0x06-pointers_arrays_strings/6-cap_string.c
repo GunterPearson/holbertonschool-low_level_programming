f7bdbd760cf77d2b2c5ba2452160d7e57752a010
GunterPearson <1715@holbertonschool.com>
2020-02-21 18:57:03 +0000

    corrected file 6 added array

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,33 +8,20 @@
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	int a[] = {'\t','\n', ',', ';', '.', '!', '?', '"', '(',
+		   ')', '{', '}', ' '};
 
 	if (s[0] >= 'a' && s[0] <= 'z')
-	{
 		s[0] -= 32;
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			switch (s[i])
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < 13; j++)
+			if (s[i] == a[j])
 			{
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-			case ' ':
-			case '\n':
-			case '\t':
-
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 					s[i + 1] -= 32;
 			}
-		}
 	}
 	return (s);
 }
