01504bc91b35aca3ed0b4820c1a8da30c39a250c
GunterPearson <1715@holbertonschool.com>
2020-02-21 21:14:50 +0000

    added file 7, 8 and header file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,5 @@
  */
 char *leet(char *s)
 {
-	int i, j;
-	char a[] = {a, A, e, E, o, O, t, T, l, L}
-	int n[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1}
-
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0, j < 10, j++)
-		{
-			if (s[i] == a[i])
-			{
-				s[i] = n[i];
-			}
-		}
-	}
 	return (s);
 }
