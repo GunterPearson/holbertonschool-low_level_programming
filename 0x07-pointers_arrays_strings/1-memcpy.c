8ee44b5c575016a491499694581b66da9ae7eee6
GunterPearson <1715@holbertonschool.com>
2020-02-24 19:29:38 +0000

    corected file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
+	while (n > 0)
 	{
-		*dest++ = *src++;
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
 	}
 	return (dest);
 }
