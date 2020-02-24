8ded9c43c23c40d3072edbd93b6609a0701d72ae
GunterPearson <1715@holbertonschool.com>
2020-02-24 15:57:06 +0000

    added mandatory files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * *_memset - start of function
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ *
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	retrun (s)
+}
