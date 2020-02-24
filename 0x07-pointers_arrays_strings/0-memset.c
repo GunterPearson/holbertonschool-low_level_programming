3375cae3ec4b55a6d790b9088d3fda58479d5b76
GunterPearson <1715@holbertonschool.com>
2020-02-24 19:17:58 +0000

    corrected 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (n > 0)
 	{
-		*(s + i) = b;
-		if (*(s + i) == '\0')
-			break;
+		s[i] = b;
+		i++;
+		n--;
 	}
+
 	return (s);
 }
