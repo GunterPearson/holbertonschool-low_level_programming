976105cb2b3e2de7acb589b6ea3bff1dfdd7b9aa
GunterPearson <1715@holbertonschool.com>
2020-02-25 22:23:12 +0000

    added file 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n > 0)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
+		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
