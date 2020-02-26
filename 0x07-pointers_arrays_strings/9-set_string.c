e142c8dd29818b5d16790135e33e06c6100f751d
GunterPearson <1715@holbertonschool.com>
2020-02-26 00:21:20 +0000

    added file 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - start of function
+ * @s: double pointer
+ * @to: pointer
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
