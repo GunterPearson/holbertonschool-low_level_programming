a3f8c82383b594c835c739b137b1c22ee858f24a
GunterPearson <1715@holbertonschool.com>
2020-02-28 20:52:08 +0000

    program prints number of arguments not including function name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @argc: count of arguments
+ * @argv: arguments passed
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
