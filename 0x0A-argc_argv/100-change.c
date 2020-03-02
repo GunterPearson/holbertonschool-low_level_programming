e3d45d4b9ac00a57fbba085f84e53b62c22c7452
GunterPearson <1715@holbertonschool.com>
2020-03-02 00:18:34 +0000

    added corrections to file 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,37 +10,44 @@
  */
 int main(int argc, char **argv)
 {
-	int i = 1;
-	int sub = atoi(argv[1]);
+	int i = 0;
+	int sub;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	while (atoi(argv[1]) - 25 >= '0')
+	if (atoi(argv[1]) < 0)
 	{
-		sub -= 25;
-		i++;
+		printf("0\n");
+		return (0);
 	}
-	while (atoi(argv[1]) - 10 >= '0')
+	sub = atoi(argv[1]);
+	if (sub > 0)
 	{
-		sub -= 10;
-		i++;
-	}
-	while (atoi(argv[1]) - 5 >= '0')
-	{
-		sub -= 5;
-		i++;
-	}
-	while (atoi(argv[1]) - 1 >= '0')
-	{
-		sub -= 1;
-		i++;
-	}
-	if (sub == 0)
-	{
+		while (sub - 25 >= 0)
+		{
+			sub -= 25;
+			i++;
+		}
+		while (sub - 10 >= 0)
+		{
+			sub -= 10;
+			i++;
+		}
+		while (sub - 5 >= 0)
+		{
+			sub -= 5;
+			i++;
+		}
+		while (sub - 1 >= 0)
+		{
+			sub -= 1;
+			i++;
+		}
 		printf("%d\n", i);
 		return (0);
 	}
+	return (0);
 }
