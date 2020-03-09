5f1c39aa7917d2a8d1f030d1c08d39d950b8b6f6
GunterPearson <1715@holbertonschool.com>
2020-03-09 20:14:42 +0000

    created print file and updated header file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,11 @@ void print_dog(struct dog *d)
 		return;
 	if ((*d).name == NULL)
 		(*d).name = "(nil)";
-	if ((*d).age == 0)
+	if ((*d).age <= 0)
+	{
 		printf("Name: %s\nAge: (nil)\nOwner: %s\n", (*d).name, (*d).owner);
+		return;
+	}
 	if ((*d).owner == NULL)
 		(*d).owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
