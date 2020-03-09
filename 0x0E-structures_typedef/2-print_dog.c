84331916165cea08b98bdb33c656af93d236e67a
GunterPearson <1715@holbertonschool.com>
2020-03-09 20:10:32 +0000

    created print file and updated header file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#define NULL 0
+#include <stdio.h>
+/**
+ * print_dog - start of funtion
+ * @d: dog struct format pointer
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).age == 0)
+		printf("Name: %s\nAge: (nil)\nOwner: %s\n", (*d).name, (*d).owner);
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
