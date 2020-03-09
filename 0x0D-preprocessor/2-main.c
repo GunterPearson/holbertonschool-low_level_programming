0ad82f584d81c0b6112c73cd20d432c80087eb67
GunterPearson <1715@holbertonschool.com>
2020-03-09 17:37:59 +0000

    created progrma that prints its name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - start of program
+ * @argc: count of arguments
+ * @argv: arguments given
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	puts(argv[0]);
+
+	return (0);
+}
