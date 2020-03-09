230ca325dff896a4e2d46fb2894eb4052fde9dd0
GunterPearson <1715@holbertonschool.com>
2020-03-09 19:30:41 +0000

    created dog header file and added struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_
+#define DOG_
+/**
+ * struct dog - new struct
+ * @name: name given
+ * @age: age given
+ * @owner: who owns it
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
