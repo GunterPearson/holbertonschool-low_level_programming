196cf5e84b408c675df1c6c1be49a98b354697c8
GunterPearson <1715@holbertonschool.com>
2020-03-11 18:40:09 +0000

    created file 1 and pushed and also updated header file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - start of funtion
+ * @array: array given
+ * @size: unsigned int
+ * @action: funtction pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
