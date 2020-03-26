85cded6434f3439ab426dc2ecb32b9340460ef54
GunterPearson <1715@holbertonschool.com>
2020-03-26 23:45:09 +0000

    corrected for edge case

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,16 +9,18 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int temp = 1;
+	unsigned int in = index;
 
 	if (index > 64)
 		return (-1);
 	if (*n == 0)
 		return (1);
-	while (index > 0)
+	while (in > 0)
 	{
 		temp <<= 1;
-		index--;
+		in--;
 	}
-	*n ^= temp;
+	if ((*n >> index) & 1)
+		*n ^= temp;
 	return (1);
 }
