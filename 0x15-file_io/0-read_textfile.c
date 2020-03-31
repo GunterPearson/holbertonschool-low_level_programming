9fdbe03aed7bc92dcd2cfec325d4700559affb20
GunterPearson <1715@holbertonschool.com>
2020-03-31 20:02:37 +0000

    corrected problems with read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,8 +14,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	if (letters == 0)
-		return (0);
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
@@ -26,18 +24,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	r = read(fd, buff, letters);
-	if (r == -1)
-	{
-		free(buff);
-		return (0);
-	}
 	w = write(1, buff, r);
-	if (w == -1 || w != r)
+	if (w != r)
 	{
 		free(buff);
 		return (0);
 	}
 	free(buff);
-	close(fd);
+	r = close(fd);
+	if (r == -1)
+		return (0);
 	return (w);
 }
