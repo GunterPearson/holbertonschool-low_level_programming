12bc785b714bd1993750f30bc089d6733768bd86
GunterPearson <1715@holbertonschool.com>
2020-03-31 19:53:26 +0000

    corrected problems with read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,15 @@
 #include "holberton.h"
-
+/**
+ * read_textfile - start of function
+ * @filename: char *
+ * @letters: number to be printed
+ *
+ * Return: number printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t r, w, fd;
+	ssize_t r, w;
+	int fd;
 	char *buff;
 
 	if (filename == NULL)
@@ -22,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buff);
 		return (0);
 	}
-	w = write(1, buff, letters);
+	w = write(1, buff, r);
 	if (w != r)
 	{
 		free(buff);
