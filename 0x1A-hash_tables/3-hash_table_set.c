6c2a183eb55dd81b34cb106817364456ab1b5d7d
GunterPearson <1715@holbertonschool.com>
2020-05-29 21:41:35 +0000

    pushing file 3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,13 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
-	check = ht->array[index];
-	if (check == NULL)
-		check = node;
-	else
-	{
-		node->next = check;
-		check = node;
-	}
+	ht->array[index] = node;
 	return (1);
 }
