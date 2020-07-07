deeb7e0984cb39b1247ac72516d96e9faf7fd850
GunterPearson <1715@holbertonschool.com>
2020-07-07 17:36:48 +0000

    pushing file 5-island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """ returns island perimeter"""
 
+
 def island_perimeter(grid):
     """ uses grid to count perimeter of island"""
     result = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
+    len1 = len(grid)
+    len2 = len(grid[0])
+    x = 0
+    while x < len1:
+        y = 0
+        while y < len2:
             if grid[x][y] == 1:
-                result += 1
-    if result != 0:
-        result += 1
-    return result * 2
+                if grid[x + 1][y] == 0:
+                    result += 1
+                if grid[x - 1][y] == 0:
+                    result += 1
+                if grid[x][y - 1] == 0:
+                    result += 1
+                if grid[x][y + 1] == 0:
+                    result += 1
+            y += 1
+        x += 1
+    return result
