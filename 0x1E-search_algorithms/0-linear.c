e208709b7fa106ecc3fefb9595f1b91dbb45a5a3
GunterPearson <1715@holbertonschool.com>
2020-08-25 13:27:08 -0500

    pushing file 0 and 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search linear
+ * @array: pointer to array
+ * @size: size_t size of array
+ * @value: value to search for
+ *
+ * Return: int 
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t x = 0;
+
+    if (array == NULL)
+        return (-1);
+    while (x < size)
+    {
+        printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+        if (array[x] == value)
+            return (x);
+        x++;
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+/**
+ * binary_search - search binary
+ * @array: array to search
+ * @size: size of array
+ * @value: the value to search for
+ *
+ * Return: int 
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    size_t l = 0, r = size - 1, m = 0, p = 0;
+
+    if (array == NULL)
+        return (-1);
+    while (l <= r)
+    {
+        printf("Searching in array: ");
+        for (p = l; p <= r; p++)
+        {
+            printf("%d", array[p]);
+            if (p != r)
+                printf(", ");
+        }
+        printf("\n");
+        m = l + (r - l) / 2;
+        if (array[m] == value)
+            return (m);
+        if (array[m] < value)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return (-1);
+}
